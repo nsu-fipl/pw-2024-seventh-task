84d57109c2471a3a7cc6fedfd4f45af7e7a0d607
Artemiy Sartakov <sartakov.artemiy@huawei-partners.com>
2024-05-08 00:15:48 +0700

    updated readme

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from wiki_crawler import WikiCrawler
 
+
 def main():
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
